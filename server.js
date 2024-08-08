61fe6555d26e44eb310bc071a8cbe58e1163cb51
Rachit Gour <146667095+RS-233@users.noreply.github.com>
2024-08-08 23:35:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
-app.use(cors({
-   origin: '*',
-   optionsSuccessStatus: 200
-}))
+app.use(cors())
 
 // DB Connection
 connectDB();
