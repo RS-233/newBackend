7a5ca300872bd21611777bc32022c158f43ef1a7
Rachit Gour <146667095+RS-233@users.noreply.github.com>
2024-08-09 15:50:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import orderRouter from "./routes/orderRoute.js"
 const app = express()
 const port = process.env.PORT || 4000;
 const corsOptions = {
-    origin: ['https://fooddelivery-yumrunfront-main.onrender.com', `http://localhost:5173`],
+    origin: ['https://fooddelivery-yumrunfront-main.onrender.com', `https://yumrun-admin.onrender.com`],
     optionsSuccessStatus: 200,
 };
 //middleware
